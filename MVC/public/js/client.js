3fbb9196b54efc90ac686de71c1a6314f3b68f8d
Joshua McGehee <JMcGehee@Joshuas-MacBook-Pro.local>
2019-06-05 19:55:14 -0600

    adding hosting on herou

--- MVC/public/js/client.js
+++ MVC/public/js/client.js
@@ -1,5 +1,35 @@
 console.log($);
 
+// const key = AIzaSyC3-dRwU-380T2MF5h331zf8tltwcDIQiA
+//User latitude and longitude
+
+let x = navigator.geolocation;
+
+const success = (position) => {
+  // fetch coordinates
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude
+  // console.log(lat,long);
+  // feed to google API
+  let coords = new google.maps.LatLng(lat,long)
+  //Set up map
+  let mapOtions = {
+    zoom: 16,
+    center: coords,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  // Create Map
+  let map = new google.maps.Map(document.getElementById("map"), mapOptions)
+  //Creat Marker
+  let marker = new google.maps.Marker({map: map, position: coords})
+}
+
+const failure = () => {
+  console.log('Co-ordinates not available.');
+}
+
+x.getCurrentPosition(success, failure);
+
 $(() => {
-  
+
 })
--- MVC/server.js
+++ MVC/server.js
@@ -9,11 +9,15 @@ const app = express();
 const mongoose = require('mongoose');
 const sightingsController = require('./controllers/sightings'); //require speciesController
 
+/////// HEROKU PORT/////////
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/pollinator_haven';
+
 // =======================
 // MONGOOSE CONFIGURATION
 // =======================
 
-mongoose.connect('mongodb://localhost:27017/pollinator_haven', { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
   console.log('Connected to mongo');
 })
@@ -22,7 +26,7 @@ mongoose.connection.once('open', () => {
 // MIDDLEWARE
 // =======================
 app.use(express.static('public'));
-app.use('/public', express.static('public')); // I might have to change how photos are accessed. 
+app.use('/public', express.static('public')); // I might have to change how photos are accessed.
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/sightings', sightingsController); //point the species route to the controller
@@ -39,6 +43,6 @@ app.get('/', (req,res) => {
 // =======================
 // LISTENING ROUTE
 // =======================
-app.listen(3000, () => {
-  console.log("Listening for pollinator_haven");
+app.listen(PORT, () => {
+  console.log("Listening for pollinator_haven on port: ", PORT);
 })
